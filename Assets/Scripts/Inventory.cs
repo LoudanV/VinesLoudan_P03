b2ac98cbd02b02cddb4ef5fab6c801b427d760f6
LoudanV <loudanv02@gmail.com>
2022-05-03 00:17:53 -0500

    VinesLoudan_P03_Commit02
    added sprites and scripts

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    private List<Items> itemList;
+
+    public Inventory()
+    {
+        itemList = new List<Items>();
+        Debug.Log("Inventory");
+    }
+}
---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items
+{
+ public enum ItemType
+    {
+        Stick,
+        Diamond
+    }
+    public ItemType itemType;
+    public int amount;
+}
